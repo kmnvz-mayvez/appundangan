e693ed6f499116b85825bfe6c86ebbaf633a79c7
Manutama aditya <55338832+kmnvz-mayvez@users.noreply.github.com>
2024-10-18 13:54:36 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <meta name="description" content="undangan powered by mayvez">
+    <meta name="description" content="undangan create by manutama">
     <meta name="tile" content="udangan">
     <meta name="MobileOptimized" content="320">
     <meta name="csrf-token" content="{{ csrf_token() }}">
